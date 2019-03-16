d071a3e022d5bb1ecb57561e69e04e69997fd845
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-15 23:48:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,11 +24,11 @@ client.on("ready", async  => {
       if (message.content.includes('discord.gg')){
                           if(!message.channel.guild) return message.reply ('')
                       if (!message.member.hasPermissions(['MANAGE_MESSAGES'])){
-         message.channel.send('ban <@' + message.author.id + '>')
+         message.channel.send('mute <@' + message.author.id + '>')
          message.delete() 
          } 
       } 
-            if (message.content.startsWith("ban")) {
+            if (message.content.startsWith("mute")) {
                if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply();
                var member= message.mentions.members.first();
                member.ban().then((member) => {
